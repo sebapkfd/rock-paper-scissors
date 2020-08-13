cb1458c1ab0cfc238658707bbd8450d03c5796c8
Seba Soto <sebastian.pkfd@gmail.com>
2020-08-12 23:28:04 -0400

    Added colors to interface

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const containerFinal = document.querySelector('.final');
 const containerResult = document.querySelector('.result');
 const containerDetail = document.querySelector('.detail');
 const containerScore = document.querySelector('.score');
+const containerUser = document.querySelector('#user');
+const containerPc = document.querySelector('#pc');
 let wonGames = 0;
 let lostGames = 0;
 let tiedGames = 0;
@@ -9,7 +11,7 @@ let roundsPlayed = 0;
 
 function computerPlay(){
 let randomNumber = Math.floor(Math.random() * 3); 
-    let options = ['rock', 'paper', 'scissors'];
+    let options = ['Rock', 'Paper', 'Scissors'];
     res = options[randomNumber];
     return res;
 }
@@ -25,14 +27,39 @@ function resultCalc(wonGames, lostGames){
 }
 
 function refreshDetail(roundDetail){
-    const content = document.createElement('div');
-    content.classList.add('content');
-    content.textContent = roundDetail;
-    if (containerDetail.firstChild === null) {
-        containerDetail.appendChild(content);
+    const userContent = document.createElement('div');
+    userContent.classList.add('content');
+    userContent.textContent = roundDetail[1];
+    const pcContent = document.createElement('div');
+    pcContent.classList.add('content');
+    pcContent.textContent = roundDetail[2];
+
+    if (roundDetail[0]=='Lost') {
+        console.log('Lost');
+        userContent.setAttribute('style', 'background-color: rgb(238, 137, 137); color: rgb(218, 42, 42);')
+        pcContent.setAttribute('style', 'background-color: rgb(111, 180, 105); color: rgb(24, 110, 17);')
+    }else if(roundDetail[0]=='Won') {
+        console.log('Won');
+        userContent.setAttribute('style', 'background-color: rgb(111, 180, 105); color: rgb(24, 110, 17);')
+        pcContent.setAttribute('style', 'background-color: rgb(238, 137, 137); color: rgb(218, 42, 42);')
+    }else if(roundDetail[0]=='Tied') {
+        console.log('Tied');
+        userContent.setAttribute('style', 'background-color: rgb(134, 194, 218); color: rgb(34, 76, 131);')
+        pcContent.setAttribute('style', 'background-color: rgb(134, 194, 218); color: rgb(34, 76, 131);')
+    }
+
+    if (containerUser.firstChild === null) {
+        containerUser.appendChild(userContent);
     }else{
-        containerDetail.removeChild(containerDetail.firstChild);
-        containerDetail.appendChild(content);
+        containerUser.removeChild(containerUser.firstChild);
+        containerUser.appendChild(userContent);
+    }
+    
+    if (containerPc.firstChild === null) {
+        containerPc.appendChild(pcContent);
+    }else{
+        containerPc.removeChild(containerPc.firstChild);
+        containerPc.appendChild(pcContent);
     }
 }
 
@@ -51,7 +78,8 @@ function refreshScore(score){
 function cleanResults(){
     containerFinal.removeChild(containerFinal.firstChild);
     containerScore.removeChild(containerScore.firstChild);
-    containerDetail.removeChild(containerDetail.firstChild);
+    containerUser.removeChild(containerUser.firstChild);
+    containerPc.removeChild(containerPc.firstChild);
     roundsPlayed = 1;
     lostGames = 0;
     tiedGames = 0;
@@ -67,15 +95,15 @@ function showResult(){
 }
 
 function playRound(playerSelection, computerSelection){
-    let roundDetail = '';
-    if ((playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock')){
-        roundDetail = `Lost with ${playerSelection} against ${computerSelection}`;
+    let roundDetail = [];
+    if ((playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
+        roundDetail = ['Lost', playerSelection, computerSelection];
         lostGames++;
-    }else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        roundDetail = `Won with ${playerSelection} against ${computerSelection}`;
+    }else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+        roundDetail = ['Won', playerSelection, computerSelection];
         wonGames++;
     }else if (playerSelection == computerSelection){
-        roundDetail = `Tied with ${playerSelection} against ${computerSelection}`;
+        roundDetail = ['Tied', playerSelection, computerSelection];
         tiedGames++;
     }else{
         console.log('error');
